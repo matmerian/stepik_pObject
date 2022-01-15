a06fb202e1540387a5ed91e129af76d50e02f780
Valerie <matmerian@yandex.ru>
2022-01-15 21:51:00 +0300

    селектор

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     def open(self):
-=======
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-    def open(self):
->>>>>>> 17a66aff293163a5d57929d46876165dc62807e6
         self.browser.get(self.url)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-=======
-from .base_page import BasePage
-from selenium.webdriver.common.by import By
-
-class MainPage(BasePage): 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
->>>>>>> 17a66aff293163a5d57929d46876165dc62807e6
         login_link.click()
+        
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, ".nav.navbar-nav.navbar-right"), "Login link is not presented"
